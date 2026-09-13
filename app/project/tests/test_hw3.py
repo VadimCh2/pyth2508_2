b66f0c1536748889448fed7787e8eec85fe2a64e
“John <john@email.com>
2026-09-13 16:46:11 +0300

    hw4

--- app/project/tests/test_hw3.py
+++ app/project/tests/test_hw3.py
@@ -1,49 +1,36 @@
 from utils_hw3 import ShoppingCart
 
 class TestShoppingCart:
-    def test_add_item_new(self):
-        cart = ShoppingCart()
-        cart.add_item('apple', 10, 3)
-        assert cart.items == [{'name': 'apple', 'price': 10, 'quantity': 3}]
+    def test_add_item_new(self, shopingcart):
+        assert shopingcart.items == [{'name': 'apple', 'price': 10, 'quantity': 3}]
 
-    def test_add_item_existing(self):
-        cart = ShoppingCart()
-        cart.add_item('apple', 10, 3)
-        cart.add_item('apple', 15, 2)
-        assert cart.items == [{'name': 'apple', 'price': 15, 'quantity': 5}]
+    def test_add_item_existing(self, shopingcart):
+        shopingcart.add_item('apple', 15, 2)
+        assert shopingcart.items == [{'name': 'apple', 'price': 15, 'quantity': 5}]
 
-    def test_add_item_multiple_different(self):
-        cart = ShoppingCart()
-        cart.add_item('apple', 10, 3)
-        cart.add_item('bread', 20, 2)
-        assert cart.items == [
+    def test_add_item_multiple_different(self, shopingcart):
+
+        shopingcart.add_item('bread', 20, 2)
+        assert shopingcart.items == [
             {'name': 'apple', 'price': 10, 'quantity': 3},
             {'name': 'bread', 'price': 20, 'quantity': 2},
         ]
 
-    def test_remove_item_existing(self):
-        cart = ShoppingCart()
-        cart.add_item('apple', 10, 3)
-        cart.remove_item('apple')
-        assert cart.items == []
+    def test_remove_item_existing(self,shopingcart):
+        shopingcart.remove_item('apple')
+        assert shopingcart.items == []
 
-    def test_remove_item_not_existing(self):
-        cart = ShoppingCart()
-        cart.add_item('apple', 10, 3)
-        cart.remove_item('bread')
-        assert cart.items == [{'name': 'apple', 'price': 10, 'quantity': 3}]
+    def test_remove_item_not_existing(self,shopingcart):
+        shopingcart.remove_item('bread')
+        assert shopingcart.items == [{'name': 'apple', 'price': 10, 'quantity': 3}]
 
     def test_get_total_empty_cart(self):
         cart = ShoppingCart()
         assert cart.get_total() == 0
 
-    def test_get_total_single_item(self):
-        cart = ShoppingCart()
-        cart.add_item('apple', 10, 3)
-        assert cart.get_total() == 30
+    def test_get_total_single_item(self,shopingcart):
+        assert shopingcart.get_total() == 30
 
-    def test_get_total_multiple_items(self):
-        cart = ShoppingCart()
-        cart.add_item('apple', 10, 3)
-        cart.add_item('bread', 20, 2)
-        assert cart.get_total() == 70
+    def test_get_total_multiple_items(self,shopingcart):
+        shopingcart.add_item('bread', 20, 2)
+        assert shopingcart.get_total() == 70
